7c0323922e14ba00b5b5131b7a3385d76bccfe87
Minwoo <alsdn8055@naver.com>
2024-09-21 23:44:05 +0900

    [FIX] resurrect service does not initialize exp

--- src/tamagotchi/tamagotchi.service.ts
+++ src/tamagotchi/tamagotchi.service.ts
@@ -292,20 +292,10 @@ export class TamagotchiService {
       throw new ApiError('TAMAGOTCHI-0005'); // Tamagotchi가 Dead 상태가 아닐 때의 에러 처리
     }
 
-    // Experience 엔티티 가져오기
-    const experience = await this.experienceRepository.findOne({
-      where: { user_id: userId },
-    });
-
     // 부활 진행: health_status를 "HEALTHY"로 변경, happiness와 hunger를 10으로 설정
     tamagotchi.health_status = HealthStatus.HEALTHY;
     tamagotchi.happiness = 10;
     tamagotchi.hunger = 10;
-
-    // Experience의 모든 속성을 0으로 초기화
-    experience.feed = 0;
-    experience.play = 0;
-    experience.pet = 0;
 
     // Tamagotchi와 Experience 업데이트
     await this.tamagotchiRepository.update(
@@ -315,15 +305,6 @@ export class TamagotchiService {
         happiness: tamagotchi.happiness,
         hunger: tamagotchi.hunger,
         sick_at: null, // 부활 시 sick_at을 null로 초기화
-      },
-    );
-
-    await this.experienceRepository.update(
-      { user_id: userId },
-      {
-        feed: experience.feed,
-        play: experience.play,
-        pet: experience.pet,
       },
     );
 
